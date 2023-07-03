3859f0c8ba99b798a951824fbf40800a2cdfa13c
RobertMarte13 <rproject133gmail.com>
2023-07-03 11:27:57 -0400

    Agregando actualizacion en tiempo real de los comentarios y sub comentarios, ademas de arreglar algunos estilos.

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -11,34 +11,42 @@ const HomePage = ({ authId }) => {
   const [subComments, setSubComments] = useState([]);
 
   useEffect(() => {
-    getALLComments();
-  }, []);
+    setTimeout( () => {
+      getComments().then((res) => {
+        setComments(res.comment);
+        setSubComments(res.subcomment);
+        console.log(res);
+      });
+    }, 1500)
+  });
 
-  function getALLComments() {
-    getComments().then((res) => {
-      setComments(res.comment);
-      setSubComments(res.subcomment);
-      console.log(res);
-    });
-  }
+  // function getALLComments() {
+  //   getComments().then((res) => {
+  //     setComments(res.comment);
+  //     setSubComments(res.subcomment);
+  //     console.log(res);
+  //   });
+  // }
 
   return (
     <div className="content-home">
       <h1>Home</h1>
-      <div className="content-post">
-        {comments &&
-          comments.map((comment, index) => (
-            <div className="content-comments" key={index}>
-              <Comments
-                comment={comment.comment}
-                subComments={subComments}
-                username={comment.username}
-                authId={authId}
-                commentId={comment.auth_id}
-                commentIdSubComment={comment.commentIdSubComment}
-              />
-            </div>
-          ))}
+      <div className="content-main-post">
+        <div className="content-post">
+          {comments &&
+            comments.map((comment, index) => (
+              <div className="content-comments" key={index}>
+                <Comments
+                  comment={comment.comment}
+                  subComments={subComments}
+                  username={comment.username}
+                  authId={authId}
+                  commentId={comment.auth_id}
+                  commentIdSubComment={comment.commentIdSubComment}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 export const getComments = async () => {
   try {
     const res = await axios.get(
